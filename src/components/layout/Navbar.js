e595467fc7dae0dcaa8e2ec942ad05f1f7036b82
eungyungcha <eungyungcha@gmail.com>
2020-09-27 23:32:42 +0900

    react router setup

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
+// a tag대신 link 사용: state 여전히 연결(페이지 이동시 검색결과 데이터 보존)
 const Navbar = ({ icon, title }) => {
   return (
     <nav className='navbar bg-primary'>
@@ -8,6 +10,14 @@ const Navbar = ({ icon, title }) => {
         <i className={icon} />
         {title}
       </h1>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
