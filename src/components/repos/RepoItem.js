ca0cf15308a5fa5c43f29bda0c3fd036db5706e0
eungyungcha <eungyungcha@gmail.com>
2020-10-05 13:20:05 +0900

    repoitem detail

--- src/components/repos/RepoItem.js
+++ src/components/repos/RepoItem.js
@@ -7,6 +7,18 @@ const RepoItem = ({ repo }) => {
       <h3>
         <a href={repo.html_url}>{repo.name}</a>
       </h3>
+      <p>{repo.description}</p>
+      <div className='card flex '>
+        <div className='badge badge-light'>Forks: {repo.forks}</div>
+        <div className='badge badge-light'>Watchers: {repo.watchers}</div>
+        <div className='badge badge-light'>Language: {repo.language}</div>
+        <div className='badge badge-dark'>
+          {' '}
+          <a href={repo.html_url} target='_blank' className='aText'>
+            See the repo
+          </a>{' '}
+        </div>
+      </div>
     </div>
   );
 };
